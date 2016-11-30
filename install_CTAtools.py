e6c468705d40d50217ef4e512a9a618abde9c303
David Sanchez <david.andre.sanchez@gmail.com>
2016-11-30 09:07:47 +0100

    cleaning

--- install_CTAtools.py
+++ install_CTAtools.py
@@ -4,8 +4,8 @@ print "\033[34m Cloning ebltable repo\033[0m"
 os.system("git clone https://github.com/me-manu/ebltable.git")
 
 
-print "\033[34m Updating ebltable repo\033[0m"
-os.system("git pull https://github.com/me-manu/ebltable.git")
+#print "\033[34m Updating ebltable repo\033[0m"
+#os.system("git pull https://github.com/me-manu/ebltable.git")
 
 print "\033[34m Test the setup\033[0m"
 os.system("python test_setup.py")
