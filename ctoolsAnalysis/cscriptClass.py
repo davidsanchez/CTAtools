52c8aa1f90d74ff38db4c79e9aa9bea7417ea07e
barbarabias <34577662+barbarabias@users.noreply.github.com>
2018-01-16 15:46:58 +0100

    Update cscriptClass.py
    Adding def csresmap (to create the residual map).
    Do we need to specify here the parameter emin, emax, coordsys, proj, xref, yref nxpix, nypix, binsz? They are also reported in the config file.

--- ctoolsAnalysis/cscriptClass.py
+++ ctoolsAnalysis/cscriptClass.py
@@ -7,7 +7,7 @@ import ctoolsAnalysis.Loggin as Loggin
 from ctoolsAnalysis.config import get_config
 import ctoolsAnalysis.Common as Common
 
-import csobsselect,csspec,cssrcdetect
+import csobsselect,csspec,cssrcdetect,csresmap
 
 class CTA_ctools_script(Loggin.base,Common.CTA_ctools_common):
     def __init__(self,outdir='.',verbose = True):
@@ -68,7 +68,7 @@ class CTA_ctools_script(Loggin.base,Common.CTA_ctools_common):
 
         self.csspec["srcname"] = self.config["target"]["name"]
         self.csspec["enumbins"] = nbpoint
-        self.csspec["method"] = "AUTO"
+        #self.csspec["method"] = "AUTO" --> (for Barbara this option generates an error)
         self.csspec["outfile"] = "spectrum_"+self.config["target"]["name"]+".fits "
         
         if self.verbose:
@@ -109,3 +109,39 @@ class CTA_ctools_script(Loggin.base,Common.CTA_ctools_common):
         self.cssrcdetect.save()
         self.info("cssrcdetect successfuly ran")
         #-------------------detect new sources
+        
+    def csresmap(self, log=False,debug=False, **kwargs):
+        '''
+        Create resmap instance with given parameters
+        Parameters
+        ---------
+        log  : save or not the log file
+        debug  : debug mode or not. This will print a lot of information
+        '''
+        self.info("Running csspec to make the residual map")
+
+        self.csresmap = csresmap.csresmap()
+        self._fill_app( self.csresmap,log=log,debug=debug, **kwargs)
+
+        # Optionally open the log file
+        self.csresmap["logfile"] = self.config['file']["tag"]+"_resmap.log"
+
+        self.csresmap["outmap"] = self.config["target"]["name"]+"_resmap.fits "
+        self.csresmap["inmodel"] = self.config["file"]["inmodel"]
+        self.csresmap["emin"] = self.config["energy"]["emin"]
+        self.csresmap["emax"] = self.config["energy"]["emax"]
+        self.csresmap["coordsys"] = "CEL"
+        self.csresmap["proj"] = "CAR"
+        self.csresmap["xref"] = self.config["target"]["ra"]
+        self.csresmap["yref"] = self.config["target"]["dec"]
+        self.csresmap["nxpix"] = 200
+        self.csresmap["nypix"] = 200
+        self.csresmap["binsz"] = 0.02
+        self.csresmap["algorithm"] = "SIGNIFICANCE"
+                
+        if self.verbose:
+            print self.csresmap
+
+        self.csresmap.run()
+        self.csresmap.save()
+        self.info("csresmap successfuly ran")
