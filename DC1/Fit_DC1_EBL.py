342b902750081ab88fb20555d908860def620d9d
barbarabias <34577662+barbarabias@users.noreply.github.com>
2018-04-11 11:38:33 +0200

    Update Fit_DC1_EBL.py

--- DC1/Fit_DC1_EBL.py
+++ DC1/Fit_DC1_EBL.py
@@ -13,7 +13,11 @@ try:
     get_ipython().magic(u'pylab')
 except :
     pass
-
+catalog = os.getcwd()+"/AGN_Monitoring_list.dat"
+#Model = "PL"
+#Model = "LPEBL"
+Model = "PLEBL"
+#Model = "PLECEBL"
 
 try:  #conf file provided
     config = get_config(sys.argv[-1])
@@ -58,10 +62,77 @@ for ii in xrange(len(Alpha)):
     #------------------ Make the XML model
     #SOURCE SPECTRUM
     lib,doc = xml.CreateLib()
-    spec = xml.PowerLawEBL(lib,srcname,filename)
+    srcname = config["target"]["name"]
+    #SOURCE SPECTRUM
+    if Model == "PL":
+	    spec = xml.addPowerLaw1(lib,srcname,"PointSource",
+		    	    eflux=1e6*numpy.sqrt(Analyse.config["energy"]["emax"]*Analyse.config["energy"]["emin"]),
+			    flux_value=1e-14,flux_max=1000.0, flux_min=1e-5)
+
+    elif Model == "PLEBL":
+	    #### EBL
+	    filename = config["out"]+"/tau_"+srcname+".txt"
+	    filefun = open(filename,"w")
+	    for j in xrange(len(ETeV)):
+	        filefun.write(str(ETeV[j]*1e6)+" "+str(max(1e-10,numpy.exp(-Tau_values)[j]))+"\n")
+	    #------------------ Make the XML model
+	    spec = xml.PowerLawEBL(lib,srcname,filename,eflux=1e6*numpy.sqrt(Analyse.config["energy"]["emax"]*Analyse.config["energy"]["emin"]))
+	    #### EBL : end
+    elif Model == "LPEBL":
+	    #### EBL
+	    filename = config["out"]+"/tau_"+srcname+".txt"
+	    filefun = open(filename,"w")
+	    for j in xrange(len(ETeV)):
+	        filefun.write(str(ETeV[j]*1e6)+" "+str(max(1e-10,numpy.exp(-Tau_values)[j]))+"\n")
+	    #------------------ Make the XML model
+	    spec = xml.LogParabolaEBL(lib,srcname,filename,eflux=1e6*numpy.sqrt(Analyse.config["energy"]["emax"]*Analyse.config["energy"]["emin"]))
+	    #### EBL : end
+    elif Model == "PLECEBL":
+	    #### EBL
+	    filename = config["out"]+"/tau_"+srcname+".txt"
+	    filefun = open(filename,"w")
+	    for j in xrange(len(ETeV)):
+	        filefun.write(str(ETeV[j]*1e6)+" "+str(max(1e-10,numpy.exp(-Tau_values)[j]))+"\n")
+	    #------------------ Make the XML model
+	    Ecut = 3./(1+redshift)*1e6
+	    spec = xml.PowerLawExpCutoffEBL(lib,srcname,filename,eflux=1e6*numpy.sqrt(Analyse.config["energy"]["emax"]*Analyse.config["energy"]["emin"]),
+			ecut_value=Ecut,ecut_free=0)
+	    #### EBL : end
+
+
+    ra = config["target"]["ra"]
+    dec = config["target"]["dec"]
     spatial = xml.AddPointLike(doc,ra,dec)
     spec.appendChild(spatial)
     lib.appendChild(spec)
+
+    #------------------ look for close sources
+    data = numpy.genfromtxt(catalog,dtype=str,unpack=True)
+    for i in xrange(len(data[0])):
+	    srcname_cat = data[0][i]+data[1][i]
+	    ra_cat = float(data[2][i])
+	    dec_cat = float(data[3][i])
+	    z = float(data[4][i])
+	    Tau_values = tau.opt_depth(z,ETeV)
+	    r = numpy.sqrt((ra_cat-ra)**2+(dec_cat-dec)**2)
+	    if r>0.1 and r<5.:
+		    print "Add source in the FoV : ",srcname_cat," ",ra_cat," ",dec_cat
+		    if data[0][i] == "1FHL":
+			    print "Add simple PowerLaw + EBL"
+			    filename = config["out"]+"/tau_addedd_"+srcname_cat+".txt"
+			    filefun = open(filename,"w")
+			    for j in xrange(len(ETeV)):
+				    filefun.write(str(ETeV[j]*1e6)+" "+str(max(1e-10,numpy.exp(-Tau_values)[j]))+"\n")
+				    #------------------ Make the XML model
+				    spec = xml.PowerLawEBL(lib,srcname_cat,filename,eflux=1e6*numpy.sqrt(Analyse.config["energy"]["emax"]*Analyse.config["energy"]["emin"]))
+		    else:
+			    print "Add simple PowerLaw"
+			    spec = xml.addPowerLaw1(lib,srcname_cat,"PointSource",
+				    eflux=1e6*numpy.sqrt(Analyse.config["energy"]["emax"]*Analyse.config["energy"]["emin"]),
+				    flux_value=1e-14,flux_max=1000.0, flux_min=1e-5)
+		    spatial = xml.AddPointLike(doc,ra_cat,dec_cat)
+		    spec.appendChild(spatial)
+		    lib.appendChild(spec)
 
     #CTA BACKGROUND
     bkg = xml.addCTAIrfBackground(lib)
